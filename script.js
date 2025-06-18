d012386f52190efa2223562a98c1826ff3ab9947
Win546 <91315687+Win546@users.noreply.github.com>
2025-06-18 18:00:41 +0200

    Template for project
    i've added the file for the projecy

---
+++ script.js
@@ -0,0 +1,11 @@
+let body=document.getElementById("body");
+let mainDiv=createElement("div");
+
+let squareDiv=createElement("div");
+
+squareDiv.className = "square";
+
+
+for(let i=0; i<16; i++){
+mainDiv.appendChild(newContent);
+}
