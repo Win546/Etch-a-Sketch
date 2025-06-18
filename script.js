f0547576e58f57dd26b8548108f467793af3732d
Win546 <91315687+Win546@users.noreply.github.com>
2025-06-18 18:04:03 +0200

    Added js
    i've added some js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let body=document.getElementById("body");
-let mainDiv=createElement("div");
+let mainDiv=document.createElement("div");
 
-let squareDiv=createElement("div");
+let squareDiv=document.createElement("div");
 
 squareDiv.className = "square";
 
 
 for(let i=0; i<16; i++){
-mainDiv.appendChild(newContent);
+mainDiv.appendChild(squareDiv);
+console.loo(i)
 }
+
+body.appendChild(mainDiv);
