e23d443333a8936f8801cb8a8a478e40a6003300
Win546 <91315687+Win546@users.noreply.github.com>
2025-06-18 18:31:32 +0200

    JS Eventlistner
    Added event lsitner for hover color, and css change

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-let body=document.getElementById("body");
+let body = document.getElementById("body");
 
 
-let squareDiv=document.createElement("div");
+let squareDiv = document.createElement("div");
 
 squareDiv.className = "square";
 
 
-for(let i=0; i<16; i++){
-body.appendChild(squareDiv.cloneNode(1));
+for (let i = 0; i < 16; i++) {
+    body.appendChild(squareDiv.cloneNode(1));
 
 }
+
+document.addEventListener("mouseover", (e) => {
+    let hovered = e.target.classList[0];
+
+    switch (hovered) {
+        case "square":
+            e.target.classList.toggle("hover")
+
+            break;
+    }
+})
+
+document.addEventListener("mouseout", (e) => {
+    let hovered = e.target.classList[1];
+
+    switch (hovered) {
+        case "hover":
+            e.target.classList.toggle("hover")
+
+            break;
+    }
+})
